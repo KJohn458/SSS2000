4daf69546bc5decc53936b33c4fe14bea08ff3b0
Colby <colbykade626@gmail.com>
2019-07-22 16:26:34 -0500

    MonsterAI

---
+++ SSS2001/Assets/Colby Thompson/Scripts/ChaseAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseAI : MonoBehaviour
+{
+    public Transform Sprite;
+    int MoveSpeed = 4;
+    int MinDist = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Sprite.position);
+        transform.Rotate(new Vector3(0, -90, 0));
+
+        if (Vector3.Distance(transform.position, Sprite.position) >= MinDist)
+        {
+            transform.Translate(new Vector3(MoveSpeed * Time.deltaTime, 0, 0));
+        }
+    }
+}
+
