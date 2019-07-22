7bcc92758e33e94fbd34032c3a63b38f41bd94ea
David G. & Tyler L <tlac542@gmail.com>
2019-07-22 15:47:44 -0500

    Game Manager
    it manages the game. that what it do.

---
+++ SSS2001/Assets/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //number of enemies
+
+    private int monsters = 0;
+
+    //player HP
+
+    private int hp;
+
+    //ammo (eventually)
+
+    private int ammo = 6;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F12)){
+
+            SceneManager.LoadScene("g",LoadSceneMode.Single);
+
+        }
+    }
+
+    int GetHealth()
+    {
+
+        return hp;
+
+    }
+
+    void SetHP(int a)
+    {
+
+        hp = a;
+
+    }
+
+    void ManageHP(int a)
+    {
+
+        hp += a;
+
+    }
+
+    int GetMonsters()
+    {
+
+        return monsters;
+
+    }
+
+    void AddMonsters(int a)
+    {
+
+        monsters += a;
+        
+    }
+
+    void SubMonsters()
+    {
+
+        monsters --;
+
+    }
+
+   int getAmmo()
+    {
+        return ammo;
+    }
+
+   void subAmmo()
+    {
+        ammo--;
+    }
+
+   void resetAmmo()
+    {
+
+        ammo = 6;
+
+    }
+
+
+}
