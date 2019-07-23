90d9d709d91c38f1c9964fd354639749cb9036ad
Aaron Sellers <aaronsellers01@gmail.com>
2019-07-23 13:12:54 -0500

    Work in progress

---
+++ SSS2001/Assets/Life.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class life: MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform barrelEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("fire"))
+        { 
+        Rigidbody projectileInstance;
+        projectileInstance = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation);
+        projectile.AddForce(barrelEnd.up * 350f);
+      }
+    }
+}
