b05748d6191fb652b52ea376f6c6780be9ad7c19
Colby <colbykade626@gmail.com>
2019-07-23 15:06:28 -0500

    Even Better Ranged AI

--- SSS2001/Assets/Colby Thompson/Scripts/RangedAI.cs
+++ SSS2001/Assets/Colby Thompson/Scripts/RangedAI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RangedAI : MonoBehaviour
 {
+    public Transform Sprite;
     [Header("Stats")]
     public float speed;
     public float stoppingDistance;
@@ -21,7 +22,8 @@ public class RangedAI : MonoBehaviour
     }
 
     void Update() {
-        
+        transform.LookAt(Sprite.position);
+        transform.Rotate(new Vector3(0, -90, 0));
 
         if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
