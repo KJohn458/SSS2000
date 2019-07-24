0dcc85b51712aed117011842d9fa5a2dd5bc2e90
Aaron Sellers <aaronsellers01@gmail.com>
2019-07-24 14:24:20 -0500

    Updates

--- SSS2001/Assets/Kill Script (Aaron and Cody)/Scripts/Health.cs
+++ SSS2001/Assets/Kill Script (Aaron and Cody)/Scripts/Health.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public GameObject gameManager;
-    public GameManager gmScript;
-    public int MaxHP;
+    private GameObject gameManager;
+    private GameManager gmScript;
+    private int MaxHP;
     private int CurrentHP;
 
     private void Start()
