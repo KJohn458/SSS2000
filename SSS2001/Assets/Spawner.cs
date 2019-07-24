26979080d79224799cbf5e30dd8aa30ff54d56f7
Colby <colbykade626@gmail.com>
2019-07-24 14:02:49 -0500

    Better Prefabs

--- SSS2001/Assets/Spawner.cs
+++ SSS2001/Assets/Spawner.cs
@@ -8,12 +8,5 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         Instantiate(Mob, transform.position, transform.rotation);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
