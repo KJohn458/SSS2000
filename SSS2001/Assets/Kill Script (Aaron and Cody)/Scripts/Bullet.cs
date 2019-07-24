9592292ca5f1dfef9bdcd89c4524e3a176a5559a
Aaron Sellers <aaronsellers01@gmail.com>
2019-07-24 13:06:40 -0500

    Shooting

---
+++ SSS2001/Assets/Kill Script (Aaron and Cody)/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int thrust = 20;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        DestroyObjectDelayed();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb2d.AddForce(transform.up * thrust);
+    }
+    private void DestroyObjectDelayed()
+    {
+        Destroy(gameObject ,3);
+    }
+}
