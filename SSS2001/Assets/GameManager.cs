e5962016766c15412fa686643d0f5955dfdd803f
Aaron Sellers <aaronsellers01@gmail.com>
2019-07-25 16:19:15 -0500

    attempt was made

--- SSS2001/Assets/GameManager.cs
+++ SSS2001/Assets/GameManager.cs
@@ -28,7 +28,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(monsters);
         if (monsters <= 0) Debug.Log("Win");
     }
 
