df4c2f4885d7464cc45d5e064d3cb6c3d9cae3eb
Alex Prosser <apsoutherngaming07@gmail.com>
2019-07-26 11:24:35 -0500

    Portal stuff done

---
+++ SSS2001/Assets/Alex Prosser/Scripts/PortalBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortalBlock : MonoBehaviour
+{
+    private GameManager gameManager;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.GetMonsters() <= 0) {
+            Destroy(gameObject);
+            text.text = "Portal has been opened!";
+        }
+    }
+}
