698385856bf206ba913a7e53396573bd14d4f474
Alex Prosser <apsoutherngaming07@gmail.com>
2019-07-26 20:02:24 -0500

    UI looking good

---
+++ SSS2001/Assets/Alex Prosser/Scripts/Canvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvas : MonoBehaviour
+{
+    public Text health;
+    public Text ammo;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.text = "Health: " + gameManager.GetHealth();
+        ammo.text = "Ammo: " + gameManager.getAmmo();
+    }
+}
