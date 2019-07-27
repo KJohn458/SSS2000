e888481690a6b6be88e86cb080f99a3733310245
Aaron Sellers <aaronsellers01@gmail.com>
2019-07-26 19:03:41 -0500

    Fixing issue with scripts

---
+++ SSS2001/Assets/Final Game/Scripts/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 0;
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            gm.SubMonsters();
+        }
+
+    }
+
+   public void DamageEnemy()
+    {
+        health--;
+    }
+}
